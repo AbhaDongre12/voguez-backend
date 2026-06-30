81a05db2fb08199ed01b0c1378ef514d2121b117
Abha Dongre <abhakdongre@gmail.com>
2026-06-30 17:38:10 +0530

    Changed Program.cs 2

--- Program.cs
+++ Program.cs
@@ -68,7 +68,7 @@ builder.Services.AddCors(options =>
             policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
         }
     );
-
+});
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
